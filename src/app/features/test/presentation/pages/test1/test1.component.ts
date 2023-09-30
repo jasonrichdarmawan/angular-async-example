327b7d50c59b84360093a3c90855f39d5d2de627
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-09-30 20:29:43 +0700

    chore: add example

--- src/app/features/test/presentation/pages/test1/test1.component.ts
+++ src/app/features/test/presentation/pages/test1/test1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-test1',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test1.component.scss']
 })
 export class Test1Component implements OnInit {
+  data: Observable<string>;
 
-  constructor() { }
+  constructor() {
+    this.data = new Observable(obs => {
+      setTimeout(() => {
+        obs.next("Hello World");
+        obs.complete();
+      }, 1000)
+    });
+  }
 
   ngOnInit(): void {
   }
